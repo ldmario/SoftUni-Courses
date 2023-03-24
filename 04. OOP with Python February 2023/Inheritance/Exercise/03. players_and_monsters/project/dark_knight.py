344d341cba15f4432a9f771ce45fc021368a9787
mdtech <106873415+ldmario@users.noreply.github.com>
2023-03-24 16:56:10 +0100

    upload players_and_monsters problem

---
+++ 04. OOP with Python February 2023/Inheritance/Exercise/03. players_and_monsters/project/dark_knight.py
@@ -0,0 +1,9 @@
+from project.knight import Knight
+
+
+class DarkKnight(Knight):
+    def __init__(self, username, level):
+        super().__init__(username, level)
+
+    def __str__(self):
+        return f"{self.username} of type {self.__class__.__name__} has level {self.level}"
