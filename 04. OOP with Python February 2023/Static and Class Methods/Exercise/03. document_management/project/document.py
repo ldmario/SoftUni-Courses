87a4ecb7158002c4bd985e81ba5d608999682b63
mdtech <106873415+ldmario@users.noreply.github.com>
2023-04-04 11:42:23 +0200

    complete solving

--- 04. OOP with Python February 2023/Static and Class Methods/Exercise/03. document_management/project/document.py
+++ 04. OOP with Python February 2023/Static and Class Methods/Exercise/03. document_management/project/document.py
@@ -1,3 +1,4 @@
+from typing import List
 from project.category import Category
 from project.topic import Topic
 
@@ -8,20 +9,23 @@ class Document:
         self.category_id = category_id
         self.topic_id = topic_id
         self.file_name = file_name
+        self.tags: [List] = []
 
     @classmethod
     def from_instances(cls, id: int, category: Category, topic: Topic, file_name: str):
         return cls(id, category.id, topic.id, file_name)
 
     def add_tag(self, tag_content: str):
-        pass
+        if tag_content not in self.tags:
+            self.tags.append(tag_content)
 
     def remove_tag(self, tag_content: str):
-        pass
+        if tag_content in self.tags:
+            self.tags.remove(tag_content)
 
     def edit(self, file_name: str):
-        pass
+        self.file_name = file_name
 
     def __repr__(self):
-        return f"Document {self.id}: {self.file_name}; category {self.category_id}, topic {self.topic_id}, " \
-                f"tags: {''}"  # tags joined by comma and space
+        return f'Document {self.id}: {self.file_name}; categories {self.category_id}, topic {self.topic_id}, \
+tags: {", ".join(self.tags)}'  # tags joined by comma and space
--- 04. OOP with Python February 2023/Static and Class Methods/Exercise/03. document_management/project/storage.py
+++ 04. OOP with Python February 2023/Static and Class Methods/Exercise/03. document_management/project/storage.py
@@ -7,36 +7,48 @@ from project.topic import Topic
 
 class Storage:
     def __init__(self):
-        self.category: [List] = []
+        self.categories: [List] = []
         self.topics: [List] = []
         self.documents: [List] = []
 
     def add_category(self, category: Category):
-        pass
+        if category not in self.categories:
+            self.categories.append(category)
 
     def add_topic(self, topic: Topic):
-        pass
+        if topic not in self.topics:
+            self.topics.append(topic)
 
     def add_document(self, document: Document):
-        pass
+        if document not in self.documents:
+            self.documents.append(document)
 
     def edit_category(self, category_id: int, new_name: str):
-        pass
+        for category in self.categories:
+            if category.id == category_id:
+                category.name = new_name
 
     def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
-        pass
+        for topic in self.topics:
+            if topic.id == topic_id:
+                topic.edit(new_topic, new_storage_folder)
 
-    def edit_document(self, document_id: int, new_document: str):
-        pass
+    def edit_document(self, document_id: int, new_file_name: str):
+        for document in self.documents:
+            if document.id == document_id:
+                document.edit(new_file_name)
 
     def delete_category(self, category_id: int):
-        pass
+        self.categories.remove([category for category in self.categories if category.id == category_id][0])
 
     def delete_topic(self, topic_id: int,):
-        pass
+        self.topics.remove([topic for topic in self.topics if topic.id == topic_id][0])
 
     def delete_document(self, document_id: int):
-        pass
+        self.documents.remove(self.get_document(document_id))
+
+    def get_document(self, document_id: int):
+        return [document for document in self.documents if document.id == document_id][0]
 
     def __repr__(self):
-        pass
+        return "\n".join(str(document) for document in self.documents)
