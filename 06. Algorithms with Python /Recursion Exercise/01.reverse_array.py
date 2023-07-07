705fe237b40b5c437976a383d799b8978ef172fe
mdtech <106873415+ldmario@users.noreply.github.com>
2023-07-08 01:19:20 +0300

    Update 01.reverse_array.py

--- 06. Algorithms with Python /Recursion Exercise/01.reverse_array.py
+++ 06. Algorithms with Python /Recursion Exercise/01.reverse_array.py
@@ -1,12 +1,24 @@
 array = list(map(int, input().split(' ')))
 
 
-def reverse(numbers, reversed_numbers):
-    if len(numbers) == 0:
-        return
-    reversed_numbers.append(numbers.pop())
-    reverse(numbers, reversed_numbers)
-    return ' '.join([str(x) for x in reversed_numbers])
+def reverse(array):
+    if len(array) != 0:
+        rev = array.pop(0)
+        reverse(array)
+        array.append(rev)
+    return ' '.join([str(x) for x in array])
 
 
-print(reverse(array, []))
+print(reverse(array))
+
+#
+#
+# def reverse(numbers, reversed_numbers):
+#     if len(numbers) == 0:
+#         return
+#     reversed_numbers.append(numbers.pop())
+#     reverse(numbers, reversed_numbers)
+#     return ' '.join([str(x) for x in reversed_numbers])
+#
+#
+# print(reverse(array, []))
